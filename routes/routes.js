c48daace61d88b21119cf92a49a2b113e21f3af6
MisaelValenzuela <106359137+MisaelValenzuela@users.noreply.github.com>
2022-06-10 16:17:17 -0500

    Delete routes directory

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { crearEquipo, getAll, getEquipo } from "../controllers/BlogController.js";
-const router = express.Router();
-router.get('/',getAll);
-router.get('/:Año',getEquipo);
-router.post('/',crearEquipo);
-
-export default router
